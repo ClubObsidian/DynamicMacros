8ae4bb414c7b16bae27a30ef087dad3c48206d65
virustotalop <virustotalop@gmail.com>
2021-02-01 04:30:37 -0800

    Initial code commit

---
+++ src/main/java/com/clubobsidian/dynamicmacro/MacroToken.java
@@ -0,0 +1,32 @@
+package com.clubobsidian.dynamicmacro;
+
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.clubobsidian.wrappy.ConfigurationSection;
+
+public class MacroToken implements Serializable {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -6384645527099128238L;
+
+	private Map<String, Object> macros;
+
+	public MacroToken(ConfigurationSection section) {
+		this.parse(section);
+	}
+
+	private void parse(ConfigurationSection section) {
+		this.macros = new LinkedHashMap<>();
+		for(String key : section.getKeys()) {
+			this.macros.put(key, section.get(key));
+		}
+	}
+
+	public Map<String, Object> getMacros() {
+		return this.macros;
+	}
+}
